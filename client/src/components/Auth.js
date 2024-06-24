5b52de4433858d332e95b668f7a49052aa655555
manishkumar73 <53996222+manishkumar73@users.noreply.github.com>
2024-06-24 16:49:56 -0700

    minor change in auth.js

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -33,9 +33,9 @@ const Auth = () => {
         <div className="auth-container">
             <form onSubmit={handleSubmit}>
                 <h2>{isSignup ? 'Sign Up' : 'Sign In'}</h2>
-                <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-                <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} required />
-                <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} required />
+                {isSignup && <input type="text" name="username" placeholder="Username" onChange={handleChange} required />}
+                {isSignup && <input type="text" name="firstName" placeholder="First Name" onChange={handleChange} required />}
+                {isSignup && <input type="text" name="lastName" placeholder="Last Name" onChange={handleChange} required />}
 
                 <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
                 <input type="password" name="password" placeholder="Password" onChange={handleChange} required />         
